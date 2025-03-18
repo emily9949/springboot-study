f3159e0e73c3fa024a76961f7e1a83926866d5f3
Sumin <emily9949@naver.com>
2025-03-18 15:23:48 +0900

    REST API 예제 추가

---
+++ chap08-rest-api-lecture-source/src/main/java/com/sumin/restapi/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.sumin.restapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/image/**")
+                .addResourceLocations("file:C:\\Users\\Sumin\\Desktop\\test.jpg");
+    }
+}
